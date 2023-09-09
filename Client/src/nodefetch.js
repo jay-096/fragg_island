87808a0cecd0a024a2e7af6ec9c46b32eaa585b6
Jay Mehta <mehtajay096@gmail.com>
2023-09-09 18:15:31 +0530

    first committ

---
+++ Client/src/nodefetch.js
@@ -0,0 +1,24 @@
+/* const fetch = require('node-fetch');
+
+const apiUrl = 'https://apiv2.shiprocket.in/v1/external/auth/login';
+
+const postData = {
+  email: 'bhojanirachit@gmail.com',
+  password: 'rachit28'
+};
+
+fetch(apiUrl, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(postData)
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log('Post created:', data);
+  })
+  .catch(error => {
+    console.error('Error creating post:', error);
+  }); */
+
---
+++ server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+
+//configure env
+dotenv.config();
+
+//databse config
+connectDB();
+
+//rest object
+const app = express();
+const bodyParser = import('body-parser');
+
+//middelwares
+app.use(cors());
+app.use(express.json());
+//app.use(bodyParser.json);
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce app</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
+
+// Login Authentication ShipRocket
+
+app.post('/api/auth/login', (req, res) => {
+  const token = generateAuthToken(); // Replace with your actual token generation logic
+  res.json({ token });
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3000');
+});
